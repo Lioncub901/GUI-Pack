eee33975cd80040048ea448eaff0eeb88d00825a
Lioncub901 <88890833+Lioncub901@users.noreply.github.com>
2025-09-02 11:18:02 -0400

    yo

--- hStack.lua
+++ hStack.lua
@@ -11,7 +11,7 @@ function gui.hstack:created(padding, spacing, stretchChildren)
     end
     self.spacing = spacing or 5
     self.stretchChildren = stretchChildren or false
-
+    
     
     self.oldSize = nil
     self.oldNum = nil
@@ -19,8 +19,8 @@ end
 
 function gui.hstack:start()
     self:stretchItems()
-   
-
+    
+    
 end
 
 function gui.hstack:setPadding(padding)
@@ -44,12 +44,12 @@ end
 function gui.hstack:stretchItems()
     local e = self.entity
     local sp = self.spacing
-
-        
+    
+    
     local stretchSize = self.entity.size.x
     local numberOfStretchChildren = 0
     local numActiveChildren = 0
-        
+    
     for i = 1, e.childCount do
         local child = e:childAt(i)
         if child.active and child.fakeParent == nil then
@@ -63,7 +63,7 @@ function gui.hstack:stretchItems()
         end
     end
     stretchSize = stretchSize - (self.padding.left + self.padding.right) - ((numActiveChildren - 1) * sp) 
-        
+    
     for i = 1, e.childCount do
         local child = e:childAt(i)
         if child.active and child.fakeParent == nil then            
@@ -71,21 +71,10 @@ function gui.hstack:stretchItems()
                 child.size = vec2(stretchSize / numberOfStretchChildren, e.size.y - (self.padding.top + self.padding.bottom))
             else
                 child.size = vec2(child.size.x, e.size.y - (self.padding.top + self.padding.bottom))
-            end
-        --[=[    
-            local stack = nil
-            if child:has(gui.vstack) then
-                stack = child:get(gui.vstack)
-            elseif child:has(gui.hstack) then
-                stack = child:get(gui.hstack)
-            end
-            if stack then
-                stack:stretchItems()
-            end]=]
-                
+            end 
         end
     end
-
+    e.shouldUpdateStack = nil
 end
 
 function gui.hstack:checkActiveCount()
@@ -101,13 +90,12 @@ function gui.hstack:checkActiveCount()
     return num
 end
 
---computeSize
 function gui.hstack:checkSize()
     if not self.stretchChildren then
         local e = self.entity
         local pd = self.padding
         local sp = self.spacing
-
+        
         local size = e.size
         size.x = self.padding.left
         
@@ -121,35 +109,8 @@ function gui.hstack:checkSize()
         
         e.size = size
     end
-   
+    
 end
-
---[[function gui.hstack:layout()
-    self:stretchItems()
-    
-    local e = self.entity
-    local sp = self.spacing
-    local x = self.padding.left
-    
-
-    
-    for i = 1, e.childCount do
-        local child = e:childAt(i)
-        if child.active and child.fakeParent == nil then            
-            child.x = x
-            child.y = (self.padding.bottom / 2) + (-self.padding.top / 2)
-            child.pivot = vec2(0, 0.5)
-            child:anchor(LEFT, STRETCH)
-            local s = child.size
-            
-            child.size = vec2(child.size.x, e.size.y - (self.padding.top + self.padding.bottom))
-            
-            x = x + s.x + ((i < e.childCount) and sp or 0)
-        end
-    end
-    x = x + self.padding.right
-    self:checkSize()
-end]]
 
 function gui.hstack:layout()
     local e = self.entity
@@ -171,7 +132,7 @@ function gui.hstack:layout()
     self.oldNum ~= childCount or
     self.activeCount ~= self:checkActiveCount() or
     (firstChild and firstChild.active and not firstChild.fakeParent and
-    (firstChild.x ~= left or firstChild.size.y ~= height))
+    (firstChild.x ~= left or firstChild.size.y ~= height)) or e.shouldUpdateStack
     )
     
     if not shouldChange then
